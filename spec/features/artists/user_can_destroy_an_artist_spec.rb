6275e4aa88fff135203937e0f137eb9fc5418ca9
Nikhil Shahi <shniks@gmail.com>
2022-08-03 09:41:17 -0700

    Created test to delete an artist.

---
+++ spec/features/artists/user_can_destroy_an_artist_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'When I visit the Artists index page' do
+  it 'I can delete an artist by clicking the delete button' do
+
+    artist_1 = Artist.create!(name: 'Pablo Picasso')
+    artist_2 = Artist.create!(name: 'Frida Kahlo')
+
+    visit '/artists'
+
+    within '.artist_2' do
+      click_button 'Delete'
+    end
+
+    expect(current_path).to eq('/artists')
+    expect(page).to have_content(artist_1.name)
+    expect(page).to_not have_content(artist_2.name)
+  end
+end
