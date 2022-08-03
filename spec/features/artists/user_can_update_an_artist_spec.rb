87d88b8735e131ec463dc7ee4309e8941d164958
Nikhil Shahi <shniks@gmail.com>
2022-08-03 10:09:06 -0700

    Created test to update an artist.

---
+++ spec/features/artists/user_can_update_an_artist_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'When I visit the artists Index' do
+  it 'I can update an artist' do
+
+    artist_1 = Artist.create!(name: 'Pablo Picasso')
+    artist_2 = Artist.create!(name: 'Frida Kahlo')
+
+    visit '/artists'
+    within '.artist_2' do
+      click_link 'Edit'
+    end
+
+    expect(current_path).to eq("/artists/#{artist_2.id}/edit")
+
+    fill_in 'name', with: 'Banksy'
+    click_on 'Update Artist'
+
+    expect(current_path).to eq('/artists')
+    within '.artist_2' do
+      expect(page).to have_content('Banksy')
+    end
+  end
+end 
